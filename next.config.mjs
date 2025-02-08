719b5b5cc3a85921e56c159b7375a8401c39e89c
Sujeev Gyawali <sujeevgyawali@Sujeevs-MacBook-Air.local>
2025-02-08 10:17:03 +0530

    Fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: 'build',
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
+    // distDir: 'build',
+    // reactStrictMode: true,
+    // output: 'export',
+    // images: {
+    //     unoptimized: true,
+    // },
 };
 
 export default nextConfig;
